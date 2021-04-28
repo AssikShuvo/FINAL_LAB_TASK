81bb30436f27ad573ab03d56b80d56429d809c72
AssikShuvo <assikuzzaman@yahoo.com>
2021-04-28 17:14:00 +0600

    Digital Diary Delete Content

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import '../../src/Note.css'
 
+
+    
+
 function Note(props){
-    return <div className="note">
+
+    function handleDelete(){
+        props.onDelete(props.id);
+    }
+
+    return ( <div className="note">
         <h2>{props.title}</h2>
         <p>{props.content}</p>
+        <button onClick={handleDelete} >Delete</button>
     </div>
+    );
 }
 
 export default Note
